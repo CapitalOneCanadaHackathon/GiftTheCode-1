1c405e37276c334ffa817e4949af17b3ace31653
feier-lai <feierlai04@gmail.com>
2017-11-04 15:21:00 -0400

    inventory page

---
+++ page-templates/template-back.php
@@ -0,0 +1,104 @@
+<?php 
+
+/*Template Name: Volunteers - backend */
+
+get_header();
+?>
+
+<div class="container marginbottom-large">
+	<div class="row">
+		<div class="col-md-7">
+			<h2>Lorem Ipsum</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
+	</div>
+
+	<div class="row marginbottom-med">
+		<div class="col-md-12 items-cat-container">
+			<button>all</button>
+			<button>food</button>
+			<button>toiletries</button>
+			<button>clothing</button>
+			<button>electronics</button>
+			<input type="search" placeholder="search something...">
+		</div>
+	</div>
+
+	<div class="row inventory-item-container">
+		<div class="col-md-6">
+			ITEM
+		</div>
+		<div class="col-md-6">
+			<div class="flex">
+				<button class="color-need">NEED</button>
+				<button class="color-want">WANT</button>
+				<button>NO NEED</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="row inventory-item-container">
+		<div class="col-md-6">
+			<div class="row">
+				<div class="col-md-12 inventory-item-label" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">ITEM</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 inventory-item-details collapse" id="collapseExample">
+				+ details<br>
+				+ details<br>
+				+ details
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="flex">
+				<button class="color-need">NEED</button>
+				<button class="color-want">WANT</button>
+				<button>NO NEED</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="row inventory-item-container">
+		<div class="col-md-6">
+			ITEM
+		</div>
+		<div class="col-md-6">
+			<div class="flex">
+				<button class="color-need">NEED</button>
+				<button class="color-want">WANT</button>
+				<button>NO NEED</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="row inventory-item-container">
+		<div class="col-md-6">
+			ITEM
+		</div>
+		<div class="col-md-6">
+			<div class="flex">
+				<button class="color-need">NEED</button>
+				<button class="color-want">WANT</button>
+				<button>NO NEED</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="row inventory-item-container">
+		<div class="col-md-6">
+			ITEM
+		</div>
+		<div class="col-md-6">
+			<div class="flex">
+				<button class="color-need">NEED</button>
+				<button class="color-want">WANT</button>
+				<button>NO NEED</button>
+			</div>
+		</div>
+	</div>
+
+
+</div>
